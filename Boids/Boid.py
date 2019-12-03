2e81bf9bf20ecbd157f8e6a2ff1f2d9935ad6a42
LeopoldBriand <lebriand@boccard.fr>
2019-12-03 17:18:31 +0100

    FIxing display of Boids on canvas

--- Boids/Boid.py
+++ Boids/Boid.py
@@ -5,19 +5,21 @@ from random import random
 
 class Boid():
 
-    def __init__(self, x, y, width, height, parent):
+    def __init__(self, x, y, parent):
+        self.parent = parent
         self.position = Vector(x, y)
         velVector = ((random() - 0.5)*10,(random() - 0.5)*10)
         accVector = ((random() - 0.5)/2,(random() - 0.5)/2)
         self.velocity = Vector(*velVector)
-        self.acceleration = Vector(*accVector)  
+        self.acceleration = Vector(*accVector)
+        self.create()
     
     def create(self):
-        self.body = canvas.create_oval(self.position.x+3, self.position.y+3,self.position.x-3, self.position.y-3)
+        self.body = self.parent.canvas.create_oval(self.position.x+3, self.position.y+3,self.position.x-3, self.position.y-3, fill='white')
     
     def update(self,parent):
         self.position += self.velocity
         self.velocity += self.acceleration
-        parent.canvas.move
+        self.parent.canvas.move(self.body,self.position.x,self.position.y)
     
     
--- Boids/Page.py
+++ Boids/Page.py
@@ -13,9 +13,10 @@ class Page():
         self.draw()
     def setup(self):
         self.canvas = Canvas(self.master, width=1000, height=1000, background='black')
+        self.canvas.pack()
     def draw(self):
         for boid in self.flock :
             boid.update(self)
     def addBoid(self):
-        self.flock.append(Boid(random()*1000,random()*1000, 1000, 1000, self))
-        self.flock[-1].update(self)
+        self.flock.append(Boid(random()*1000,random()*1000, self))
+        #self.flock[-1].update(self)
